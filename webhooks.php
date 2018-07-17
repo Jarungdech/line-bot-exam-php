5b0d7b7dae3a24f34ec1a6ea0d454d8b26b10958
Jarungdech <36566454+Jarungdech@users.noreply.github.com>
2018-07-17 11:19:18 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'btjZR0GhB+o+mLzZ7ggAq9JmqRHInFb1eJdqLeL/mBmsRAxVpHIF318ds7pIObPC1S1iMajLIjKMcsrRU95+7SIq9AXHDhOboaRBJ9et+FBWEiRzM9Q8nF490FzDI/sEg6y9Bb7DIxv23CvsAPNlcAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
